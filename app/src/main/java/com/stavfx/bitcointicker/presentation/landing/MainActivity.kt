5602c040a3e48fa1317174613ab86304fdffc343
Stav Raviv <stav.raviv@avast.com>
2021-02-06 11:31:15 -0800

    moved MainActivity to a better place

--- app/src/main/java/com/stavfx/bitcointicker/presentation/landing/MainActivity.kt
+++ app/src/main/java/com/stavfx/bitcointicker/presentation/landing/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.stavfx.bitcointicker
+package com.stavfx.bitcointicker.presentation.landing
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.stavfx.bitcointicker.R
 
 class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
